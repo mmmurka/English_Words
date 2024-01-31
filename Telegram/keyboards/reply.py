3c8a01610b0037894e7d9feda801d09a1b66df82
Nemsh <79201622+BillShalker@users.noreply.github.com>
2024-01-31 19:50:58 +0100

    Something updated

--- Telegram/keyboards/reply.py
+++ Telegram/keyboards/reply.py
@@ -3,7 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 main_kb = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text='242'),
+            KeyboardButton(text='243'),
             KeyboardButton(text='Автори')
         ],
         [
