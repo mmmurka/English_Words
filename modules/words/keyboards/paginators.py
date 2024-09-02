d9ada35daa8a917941d07c0ae6e87b317ff51a7a
Nemsh <Artnemshilov@gmail.com>
2024-09-02 22:54:17 +0200

    group subject paginator will be only if len(elements) > 10

--- modules/words/keyboards/paginators.py
+++ modules/words/keyboards/paginators.py
@@ -1,6 +1,7 @@
 from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
 # from layers.functions.cb_encoder import encode_table, encode_group_subject
 from modules.words.data.data_retriever import get_group_subjects
 
@@ -37,19 +38,25 @@ async def handle_pagination_action(action: str, page: int, total_pages: int) -> 
 async def create_subject_paginator(table_name: str):
     groups_of_subject = await get_group_subjects(table_name)
     groups_of_subject = list(map(lambda x: x.capitalize(), groups_of_subject))
-    groups_of_subject = [groups_of_subject[i:i + 10] for i in range(0, len(groups_of_subject), 10)]
+    count_groups = len(groups_of_subject)
+    if count_groups > 10:
+        groups_of_subject = [groups_of_subject[i:i + 10] for i in range(0, len(groups_of_subject), 10)]
     def group_subject_paginator(page: int = 0):
         builder = InlineKeyboardBuilder()
-        [builder.button(text=group_subject, callback_data='None') for group_subject in groups_of_subject[page]]
-        builder.adjust(1)
-        builder.row(
-            InlineKeyboardButton(text="⬅️", callback_data=GroupSubjectPagination(action="prev", page=page,
-                                                                                 db_table=table_name).pack()),
-            InlineKeyboardButton(text="➡️", callback_data=GroupSubjectPagination(action="next", page=page,
-                                                                                 db_table=table_name).pack()),
-            InlineKeyboardButton(text="Назад", callback_data='word_tables'),
-            width=2
-        )
+        if count_groups > 10:
+            [builder.button(text=group_subject, callback_data='None') for group_subject in groups_of_subject[page]]
+            builder.adjust(1)
+            builder.row(
+                InlineKeyboardButton(text="⬅️", callback_data=GroupSubjectPagination(action="prev", page=page, db_table=table_name).pack()),
+                InlineKeyboardButton(text="➡️", callback_data=GroupSubjectPagination(action="next", page=page, db_table=table_name).pack()),
+                InlineKeyboardButton(text="Назад", callback_data='word_tables'),
+                width=2)
+
+        else:
+            [builder.button(text=group_subject, callback_data='None') for group_subject in groups_of_subject]
+            builder.button(text="Назад", callback_data='word_tables')
+            builder.adjust(1)
+
         return builder.as_markup()
 
     return group_subject_paginator
