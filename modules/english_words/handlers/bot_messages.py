50aee2640310250ee7979dae0c94253ed2ca0252
Nemsh <Artnemshilov@gmail.com>
2024-09-05 00:09:59 +0200

    deleted old english_words_module

--- modules/english_words/handlers/bot_messages.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router
-from aiogram.types import Message
-
-router = Router()
-
-
-@router.message()
-async def echo(message: Message):
-    await message.answer('Оберіть необхідний пункт меню ❤️')
