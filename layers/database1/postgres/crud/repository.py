b5d2174b5758dbee86f356950400025e8f1c3341
Nemsh <Artnemshilov@gmail.com>
2024-10-08 20:07:55 +0200

    repo pattern update

--- layers/database1/postgres/crud/repository.py
+++ layers/database1/postgres/crud/repository.py
@@ -1,50 +1,68 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from postgres.protocols.abstract_repository import IRepository
-from typing import Any, Type, Callable, Coroutine
+from logging import critical
+from typing import Type, Any, Optional, Callable, Sequence
+
+from sqlalchemy import Select
+
 from postgres.models import DynamicTable
+from postgres.sql.repository import SqlAlchemyRepository
 
 
-class CRUDPostgresRepository(IRepository[AsyncSession]):
-    def __init__(self, session_factory: Callable[[], Coroutine[Any, Any, AsyncSession]]):
-        self._session_factory = session_factory
-
-    @property
-    async def async_session(self) -> AsyncSession:
-        return await self._session_factory()
-
-    async def get_all(self, entity: Any):
-        async with self.async_session as session:
-            result = await session.execute(select(entity))
-            return result.scalars().all()
-
-    async def get_by_id(self, entity: Any, id: str):
-        async with self.async_session as session:
-            result = await session.execute(select(entity).where(entity.id == id))
-            return result.scalar_one_or_none()
-
-    async def add(self, entity: Any):
-        async with self.async_session as session:
-            session.add(entity)
-            await session.commit()
-
-    async def update(self, entity: Any):
-        async with self.async_session as session:
-            await session.merge(entity)
-            await session.commit()
-
-    async def delete(self, entity: Any, id: str):
-        async with self.async_session as session:
-            existing_entity = await self.get_by_id(entity, id)
-            if existing_entity:
-                await session.delete(existing_entity)
+class Repository(SqlAlchemyRepository):
+    async def saveChanges(self):
+        async with (await self.async_session) as session:
+            try:
                 await session.commit()
+            except Exception as err:
+                critical(err)
+                await session.rollback()
+                raise err
+
+    def buildQuery(self, *entities: Any) -> Select:
+        return Select(*entities)
+
+    async def fetch(self, *entities: Any, modifier: Optional[Callable[[Any], Any]] = None) -> Sequence[Any]:
+        query = self.buildQuery(*entities)
+        if modifier is not None:
+            query = modifier(query)
+
+        async with (await self.async_session) as session:
+            result = await session.execute(query)
+            return result.unique().all()
+
+    async def fetchColumn(self, column: Any, *filters: Any) -> Sequence[Any]:
+        query = self.buildQuery(column)
+        query = query.filter(*filters).distinct()
+
+        async with (await self.async_session) as session:
+            result = await session.scalars(query)
+            return result.unique().all()
+
+
+class ReadRepository(Repository):
+
+    def _query(self, *entities: Any) -> Select:
+        return Select(*entities)
+
+    def _queryWithParams(self, *filters: Any, group: Optional[Any] = None) -> Select:
+        query = self._query()
+
+        if group is not None:
+            query = query.group_by(group)
+
+        if filters:
+            query = query.filter(*filters)
+
+        return query
+
+
+class CRUDPostgresRepository(ReadRepository):
 
     @staticmethod
     async def create_dynamic_table(table_name: str, extend_existing: bool = True) -> Type[DynamicTable]:
         """
         Creates a table class dynamically based on the DynamicTable base model.
         """
+
         class Table(DynamicTable):
             __tablename__ = table_name
             __table_args__ = {'extend_existing': extend_existing}
